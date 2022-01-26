236490118ad635205c7fa5c072fd546e21a42731
AlexWooolf <para_bellum@list.ru>
2022-01-26 14:18:47 +0300

    1.5. Поля объекта [#166083 #255459]

---
+++ src/main/java/ru/job4j/oop/Ball.java
@@ -0,0 +1,11 @@
+package ru.job4j.oop;
+
+public class Ball {
+    public static void tryRun(boolean condition) {
+        if (condition) {
+            System.out.println("RIP kolobok");
+        } else {
+            System.out.println("Wanted: dead or alive. Kolobok.");
+        }
+    }
+}
