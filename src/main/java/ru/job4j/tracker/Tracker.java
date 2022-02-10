b24b122153ed1618654285354ffc64e3e305fd07
AlexWooolf <para_bellum@list.ru>
2022-02-10 12:16:27 +0300

    5. Tracker - хранилище [#396 #262661]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -14,17 +14,7 @@ public class Tracker {
     }
 
     public Item[] findAll() {
-        Item[] rsl = new Item[size];
-        int count = 0;
-        for (int i = 0; i < size; i++) {
-            Item copy = items[i];
-            if (copy != null) {
-                rsl[count] = copy;
-                count++;
-            }
-        }
-        rsl = Arrays.copyOf(rsl, size);
-        return rsl;
+       return Arrays.copyOf(items, size);
     }
 
     public Item[] findByName(String key) {
