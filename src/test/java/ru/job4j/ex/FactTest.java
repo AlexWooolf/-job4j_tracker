1119f2aefe7ae53bc568e308a8c79cc7efbc9feb
Vovk <para_bellum@list.ru>
2022-02-18 14:04:09 +0300

    0.3. Тестирование исключений с junit [#219366]

---
+++ src/test/java/ru/job4j/ex/FactTest.java
@@ -0,0 +1,12 @@
+package ru.job4j.ex;
+
+import org.junit.Test;
+
+public class FactTest {
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenNNegative() {
+Fact test = new Fact();
+test.calc(-5);
+    }
+}
