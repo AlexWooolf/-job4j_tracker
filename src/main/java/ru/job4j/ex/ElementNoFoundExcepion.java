e6b3e3aa337def24ca8db23aa1f750111f28a460
Vovk <para_bellum@list.ru>
2022-02-20 13:28:56 +0300

    0.4. Пользовательские исключения. [#219367]

---
+++ src/main/java/ru/job4j/ex/ElementNoFoundExcepion.java
@@ -0,0 +1,7 @@
+package ru.job4j.ex;
+
+public class ElementNoFoundExcepion extends Exception {
+    public ElementNoFoundExcepion(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -0,0 +1,28 @@
+package ru.job4j.ex;
+
+import java.util.Objects;
+
+public class FindEl {
+    public static int indexOf(String[] value, String key) throws ElementNoFoundExcepion {
+        int rsl = -1;
+        for (int i = 0; i < value.length; i++) {
+            if (Objects.equals(value[i], key)) {
+                rsl = i;
+            }
+        }
+        if (rsl == -1) {
+            throw new ElementNoFoundExcepion("Элемент не найден");
+        }
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        String[] value = {"one", "three"};
+        String input = "two";
+        try {
+            System.out.println(indexOf(value, input));
+        } catch (ElementNoFoundExcepion e) {
+            e.printStackTrace();
+        }
+    }
+}
