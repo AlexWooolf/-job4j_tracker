19c10661728de35f794f114c2fcb91dd11a88b78
AlexWooolf <para_bellum@list.ru>
2022-04-06 15:49:42 +0300

    2. Отсортировать департаменты [#34352 #284420]

--- src/main/java/ru/job4j/collection/DepDescComp.java
+++ src/main/java/ru/job4j/collection/DepDescComp.java
@@ -8,8 +8,8 @@ public class DepDescComp implements Comparator<String> {
         String[] first = o1.split("/");
         String[] second = o2.split("/");
         int rsl = second[0].compareTo(first[0]);
-        rsl = rsl == 0 ? o1.compareTo(o2) : rsl;
-        return rsl;
+        return rsl == 0 ? o1.compareTo(o2) : rsl;
+
     }
 }
 
