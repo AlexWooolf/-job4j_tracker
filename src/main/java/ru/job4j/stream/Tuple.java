1d23a0724f04cade46505a4606571ba26f5d579b
AlexWooolf <para_bellum@list.ru>
2022-04-25 19:25:00 +0300

    4. Optional в Stream API. [#279130]

---
+++ src/main/java/ru/job4j/stream/Tuple.java
@@ -0,0 +1,42 @@
+package ru.job4j.stream;
+
+import java.util.Objects;
+
+public class Tuple implements Comparable<Tuple> {
+    private String name;
+    private double score;
+
+    public double getScore() {
+        return score;
+    }
+
+    public Tuple(String name, double score) {
+        this.name = name;
+        this.score = score;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Tuple tuple = (Tuple) o;
+        return Double.compare(tuple.score, score) == 0
+                &&
+                Objects.equals(name, tuple.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, score);
+    }
+
+    @Override
+    public int compareTo(Tuple o) {
+        int rsl = (int) this.getScore() - (int) o.getScore();
+        return rsl;
+    }
+}
